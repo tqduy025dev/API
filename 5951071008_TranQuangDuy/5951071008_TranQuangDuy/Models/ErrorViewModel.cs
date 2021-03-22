6706a40956808675219c20ae324c2fa803ded366
tranquangduy007 <duy05022000@gmail.com>
2021-03-22 18:00:58 +0700

    Buoi4_Phan1

---
+++ 5951071008_TranQuangDuy/5951071008_TranQuangDuy/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _5951071008_TranQuangDuy.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
