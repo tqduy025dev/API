9b8d421453c32239053facb219d85291dc13955b
tranquangduy007 <duy05022000@gmail.com>
2021-03-22 23:58:49 +0700

    all

--- FB_API/5951071008_TranQuangDuy/WebApplication1/WebApplication1/Content.cs
+++ FB_API/5951071008_TranQuangDuy/WebApplication1/WebApplication1/Content.cs
@@ -7,5 +7,12 @@ namespace WebApplication1
 {
     public class Content
     {
+        private string time;
+        private string message;
+        private string link;
+
+        public string Time { get => time; set => time = value; }
+        public string Message { get => message; set => message = value; }
+        public string Link { get => link; set => link = value; }
     }
 }
